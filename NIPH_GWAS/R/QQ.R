9514156ddadbd75e72d1f8d262081ac87ef62ad0
Christian Page <christpa@users.noreply.github.com>
2018-08-31 10:36:04 -0400

    Create QQ.R

---
+++ NIPH_GWAS/R/QQ.R
@@ -0,0 +1,11 @@
+#' QQ-plot of basic GWAS result file 
+#'
+#' @param res Result from a GWAS analysis
+#' @param col (Optional) Gives the column for the p-values. Only needed if the p-values is not stored in the 3rd column. 
+#' @return A ggplot2 object
+#' @examples
+#' QQ(res)
+QQ <- function(res, col = 3){
+
+  return(0)
+}
